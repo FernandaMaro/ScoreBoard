397a0eed490574490ef8f12f34560b7fb42e2d50
Fernanda Maro <148138579+FernandaMaro@users.noreply.github.com>
2023-12-01 15:16:16 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let count1= 0
+let count2= 0
+let scoreEl= document.getElementById("score1")
+let scoreEl2= document.getElementById("score2")
+
+function plus1(){
+    count1+=1
+    scoreEl.textContent=count1;
+}
+function plus2(){
+    count1+=2
+    scoreEl.textContent=count1;
+}
+function plus3(){
+   count1+=3
+    scoreEl.textContent=count1; 
+}
+function plusOne(){
+   count2+=1
+    scoreEl2.textContent=count2;
+}
+function plusTwo(){
+   count2+=2
+    scoreEl2.textContent=count2;
+}
+function plusThree(){
+   count2+=3
+    scoreEl2.textContent=count2;
+}
